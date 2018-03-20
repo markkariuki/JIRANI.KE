5ed7550105450b796d7f1260e5ef702a5566c0a5
david mark <markkariuki17@gmail.com>
2018-03-20 12:32:42 +0300

    refactor my app for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn​ ​ project_name(jikonis) ​ .wsgi​
--- jikonis/settings.py
+++ jikonis/settings.py
@@ -11,22 +11,24 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7zudn=u-sw+noasg%z2gw&5^g31_clggxye3r=k+v*^@2+p8j$'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 LOGIN_REDIRECT_URL='/home'
@@ -85,7 +87,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
 
+
+db_from_env=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -123,3 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+os.path.join(PROJECT_ROOT, 'static'),)
